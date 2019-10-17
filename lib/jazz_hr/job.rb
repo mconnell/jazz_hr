4fda6968a6c64b0c5e2493827b46f05c78a25cd5
Mark Connell <git@markconnell.co.uk>
2019-10-17 21:32:23 +0100

    basic find method

--- lib/jazz_hr/job.rb
+++ lib/jazz_hr/job.rb
@@ -4,11 +4,20 @@ module JazzHR
       response = client.get(path: "/jobs")
       if response.is_a? Array
         response.map do |job_attributes|
-          self.new(job_attributes)
+          new(job_attributes)
         end
       else
-        [self.new(response)]
+        [new(response)]
       end
+    end
+
+    def self.find(client:, id:)
+      raise Error.new("No job ID supplied") if id.nil?
+
+      job_attributes = client.get(path: "/jobs/#{id}")
+      return nil if job_attributes["id"] != id
+
+      new(job_attributes)
     end
 
     def initialize(attributes)
--- spec/jazz_hr/job_spec.rb
+++ spec/jazz_hr/job_spec.rb
@@ -32,4 +32,26 @@ RSpec.describe JazzHR::Job do
       end
     end
   end
+
+  describe ".find" do
+    context "no job id supplied" do
+      it "raises an error" do
+        expect {
+          JazzHR::Job.find(client: client, id: nil)
+        }.to raise_error(JazzHR::Error, "No job ID supplied")
+      end
+    end
+
+    context "invalid job id supplied" do
+      it "returns nil", :vcr do
+        expect(JazzHR::Job.find(client: client, id: 'invalid')).to be_nil
+      end
+    end
+
+    context "valid job id supplied" do
+      it "returns a job", :vcr do
+        expect(JazzHR::Job.find(client: client, id: 'job_20191017201346_RS8OXHY7X8MA9GO8')).to be_a(JazzHR::Job)
+      end
+    end
+  end
 end
